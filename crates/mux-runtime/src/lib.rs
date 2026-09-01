f99f0490d04e08dfc790a8076ce0c1da39c43176
nethum529 <nethumweerasinghe.nw@gmail.com>
2026-08-31 22:22:53 -0500

    Add user session runtime
    Signed-off-by: nethum529 <nethumweerasinghe.nw@gmail.com>

--- crates/mux-runtime/src/lib.rs
+++ crates/mux-runtime/src/lib.rs
@@ -1,7 +1,9 @@
 pub mod pane_grid;
 mod pane_host;
 mod pty;
+mod user_session;
 
 pub use pane_grid::{Cell, Color, PaneGrid};
 pub use pane_host::PaneHost;
 pub use pty::PtySession;
+pub use user_session::UserSession;
